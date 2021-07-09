56c36451e377dea78978b8f6b2dcc3c77d402871
Rubens Araújo <rubens@octosoft.com.br>
2021-07-09 20:24:05 -0300

    Adicionando EF Core no Projeto com SQLite

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,54 +13,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
-            
+            _context = context;
         }
-
-        public IEnumerable<Evento> _evento = new Evento[]
-        {
-            new Evento {
-                EventoId = 1, 
-                Tema = "Angular 11", 
-                Local = "São Paulo", 
-                Lote = "1° Lote",
-                QtdPessoas = 250, 
-                ImageURL = "foto-angular-11.png",
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyy")
-            }, 
-             new Evento {
-                EventoId = 2, 
-                Tema = "Angular 11 + ASP NET CORE 6", 
-                Local = "São Paulo", 
-                Lote = "1° Lote",
-                QtdPessoas = 250, 
-                ImageURL = "foto-angular-11-aspnet-core-5.png",
-                DataEvento = DateTime.Now.AddDays(5).ToString("dd/mm/yyy")
-            }, 
-             new Evento {
-                EventoId = 3, 
-                Tema = "Entity Framework Core", 
-                Local = "São Paulo", 
-                Lote = "1° Lote",
-                QtdPessoas = 250, 
-                ImageURL = "foto-entity-framework.png",
-                DataEvento = DateTime.Now.AddDays(10).ToString("dd/mm/yyy")
-            }, 
-
-        };
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(x => x.EventoId == id);
+            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
         }
 
         [HttpPost]
@@ -67,6 +37,6 @@ namespace ProEventos.API.Controllers
         {
             return "Retorno da API POST: " + DateTime.Now;
         }
-    
+
     }
 }
