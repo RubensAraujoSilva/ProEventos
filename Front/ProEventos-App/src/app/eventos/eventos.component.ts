cc87e2c4e0e2e2ee265c9192dff6e1e7eca750e2
Rubens Araújo <rubens@octosoft.com.br>
2021-07-22 19:34:32 -0300

    Angular, Interpolação, Diretivas e Binds

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  exibirImagem: boolean = false;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    //Lógica da Pesquisa:
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  //Método que irá filtrar nossos eventos
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase(); //Covertendo a string em caixa baixo
+
+    return this.eventos.filter(
+      (evento: {tema: string, local: string,}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -19,10 +43,16 @@ export class EventosComponent implements OnInit {
   public getEventos(): void{
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error),
     )
+  }
 
+  alternarExibicaoImg() {
+    this.exibirImagem = !this.exibirImagem;
   }
 
 }
